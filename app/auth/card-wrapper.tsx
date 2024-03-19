7578d7b64435fbe68e4c9755143e5599faa321fc
Mohammed Zaid M <codezaidprof@gmail.com>
2024-03-19 14:21:08 +0530

    Initial UI form Commit

---
+++ app/auth/card-wrapper.tsx
@@ -0,0 +1,50 @@
+"use client"
+import {Header} from '@/components/auth/header'
+import {Social} from  '@/components/auth/social'
+import {BackButton} from '@/components/auth/back-button'
+import {
+    Card,
+    CardContent, CardHeader,
+    CardFooter
+} from '@/components/ui/card'
+
+
+
+interface CardWrapperProps{
+    children: React.ReactNode;
+    headerLabel: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+};
+
+export const CardWrapper =({
+    children,
+    headerLabel,
+    backButtonLabel,
+    backButtonHref,
+    showSocial
+}: CardWrapperProps)=>{
+    return(
+        <Card className='w-[400px] shadow-md'>
+            <CardHeader>
+                <Header label={headerLabel}/>
+            </CardHeader>
+            <CardContent>
+                {children}
+            </CardContent>
+            {showSocial &&(
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+            <CardFooter>
+                <BackButton
+                    href={backButtonHref}
+                    label={backButtonLabel}
+                />
+            </CardFooter>
+        </Card>
+    )
+
+};
